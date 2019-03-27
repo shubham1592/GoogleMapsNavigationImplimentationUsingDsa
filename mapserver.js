2d845769db090d1c23f265c2ed5b7a62f4b02798
Neetigya Chahar <neetigyachahar784@gmail.com>
2019-03-27 14:14:07 +0530

    First node app deploy

---
+++ mapserver.js
@@ -0,0 +1,43 @@
+const spwan = require('child_process').spawn;
+const https = require('http');
+const url = require('url');
+const fs = require('fs');
+
+
+
+https.createServer(function (req, res){
+    var q = url.parse(req.url, true);
+    var urlobj = q.query;
+    console.log(urlobj.id);
+    if(JSON.stringify(urlobj) == "{}"){
+        var sor = req.url.slice(1);
+        console.log(urlobj.auth);
+        if(sor == ''){
+            sor = "home.html";
+       }
+           try{var daat = fs.readFileSync(sor);}
+           catch(error) {
+               console.log(error);
+               var daat = fs.readFileSync('error.html');}
+           res.writeHead(200);
+           res.write(daat);
+           res.end();
+       }
+       else{
+    switch(urlobj.id){
+        case '1': {
+                console.log("aaya");
+                const pythonProcess = spwan('python', ["dijkstra.py", urlobj.src,urlobj.dest ]);
+                pythonProcess.stdout.on('data', (data)=>{
+                console.log(String(data));
+                var arr = JSON.parse(data);
+                console.log(String(data));
+                res.write(data);
+                res.end();
+                });
+        }            
+    }
+}
+}).listen(8080);
+
+
