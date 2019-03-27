09ae615b70920e3f96cca2425ba98641cd5764c3
Neetigya Chahar <neetigyachahar784@gmail.com>
2019-03-27 18:53:54 +0530

    First node app deploy

---
+++ main.js
@@ -0,0 +1,47 @@
+var xhttp = new XMLHttpRequest();
+var data = '';
+ function callme(){
+    var list = [];
+    console.log(window.location.href);
+    var query = window.location.href+"?id=1"+"&src="+document.getElementById('source').options[document.getElementById('source').options.selectedIndex].value+
+                "&dest="+document.getElementById('destination').options[document.getElementById('destination').options.selectedIndex].value;
+        xhttp.onreadystatechange= function(){
+            if(this.readyState == 4 && this.status == 200){
+                console.log(this.responseText);
+                data = JSON.parse(this.responseText);
+                console.log("Ye Dekh: "+ data.path[0]);
+                for(var i = 0; i< data.path.length; i++){
+                    list.push({"lat": data.path[i][0], "lng": data.path[i][1]});
+                }
+                console.log(list);
+                initMap(list);
+            }
+        };
+    xhttp.open('get', query, true);
+    xhttp.send();
+        };
+
+ 
+ 
+ function initMap(locat) {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: {lat:12.971432,lng:79.160235},
+          mapTypeId: 'terrain'
+        });
+        console.log(locat);
+        var flightPlanCoordinates = locat;
+        var flightPath = new google.maps.Polyline({
+          path: flightPlanCoordinates,
+          geodesic: true,
+          strokeColor: '#FF0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 2
+        });
+        flightPath.setMap(map);
+    }
+
+function show(){
+    document.getElementById('map').style.width =  1300+'px';
+    document.getElementById('map').style.height = 830+'px';
+}
--- server.js
+++ server.js
@@ -1,47 +1,43 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var la = require('./loginAuth');
-var sql = require('mysql');
-
-var con = sql.createConnection({
-    host: "us-cdbr-iron-east-03.cleardb.net",
-    user: "bdfce9d5e367f0",   //root
-    password:"cc3917d5",    //bharat1@
-});
+const spwan = require('child_process').spawn;
+const https = require('http');
+const url = require('url');
+const fs = require('fs');
 
 
-con.query("CREATE DATABASE test",function(err, result){
-    if(err) throw err;
-    console.log("Database created");
-});
 
-http.createServer(function(req, res){
+https.createServer(function (req, res){
     var q = url.parse(req.url, true);
-    var qobj = q.query;
-    console.log(q);
-    console.log(req.url.slice(1));
-    if(JSON.stringify(qobj) == "{}"){
-     var sor = req.url.slice(1);
-     console.log(qobj.auth);
-     if(sor == ''){
-         sor = "login.html";
+    var urlobj = q.query;
+    console.log(urlobj.id);
+    if(JSON.stringify(urlobj) == "{}"){
+        var sor = req.url.slice(1);
+        console.log(urlobj.auth);
+        if(sor == ''){
+            sor = "home.html";
+       }
+           try{var daat = fs.readFileSync(sor);}
+           catch(error) {
+               console.log(error);
+               var daat = fs.readFileSync('error.html');}
+           res.writeHead(200);
+           res.write(daat);
+           res.end();
+       }
+       else{
+    switch(urlobj.id){
+        case '1': {
+                console.log("aaya");
+                const pythonProcess = spwan('python', ["dijkstra.py", urlobj.src,urlobj.dest ]);
+                pythonProcess.stdout.on('data', (data)=>{
+                console.log(String(data));
+                var arr = JSON.parse(data);
+                console.log(String(data));
+                res.write(data);
+                res.end();
+                });
+        }            
     }
-        try{var daat = fs.readFileSync(sor);}
-        catch(error) {
-            console.log(error);
-            var daat = fs.readFileSync('error.html');}
-        res.writeHead(200);
-        res.write(daat);
-        res.end();
-    }
-    else{
-        la.auth(qobj,con, function(data){
-            console.log(data);
-            res.writeHead(200, {'Content-Type': 'text/html'});        
-            res.write(data);
-            res.end();
-        });
-    }
-    
-}).listen(8080);
+}
+}).listen(process.env.PORT ||8080);
+
+
